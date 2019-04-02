0a38640ad4d1e332ec660c1a7d008511a377a3f0
kaptainmidnight <alexartemkin173@mail.ru>
2019-04-02 21:49:25 +0400

    add button markup

--- bot.py
+++ bot.py
@@ -2,23 +2,33 @@
 # -*- coding: utf-8 -*-
 
 
-from telebot import TeleBot
+from telebot import TeleBot, types
 
 token = "851940626:AAEh8TpdiGqjFtQy-aRgm36nA-HvG1A1CN4"
 
 bot = TeleBot(token)
 
 
+# @bot.message_handler(content_types=['text'])
+# def send(message):
+#     mat = ['блять', 'черт', 'пошел нахуй']
+#     response = message.text
+#     for i in mat:
+#         print(i)
+#         print(i if response in i else "Список не содержит заданного элемента")
+
+
 @bot.message_handler(content_types=['new_chat_members'])
 def greeting(message):
+    markup = types.InlineKeyboardMarkup()
+    btn_github = types.InlineKeyboardButton(text='Github', url='https://github.com/Murrengan/murr')
+    btn_trello = types.InlineKeyboardButton(text='Trello', url='https://trello.com/b/yfjytAFU/murrengan')
+    markup.add(btn_github, btn_trello)
     bot.send_message(message.chat.id, text=
-    f'Добро пожаловать, @{message.from_user.username}!\n\n'
-    f'[ Trello ](https://trello.com/b/yfjytAFU/murrengan),\n'
-    f'[ Стримы Twitch ](https://www.twitch.tv/murren_egor),\n'
-    f'[ Мой канал на Youtube ](https://www.youtube.com/murrengan),\n'
-    f'[ Мой Vk ](https://vk.com/egor.a.komarov)\n\n'
-    'Да прибудет с тобой сила!\n'
-    'Murrengan')
+    f'@{message.from_user.username}, привет!\n'
+    'Добро пожаловать в группу.\n'
+    'Ниже основные штуки по проекту.\n'
+    'И да пребудет с тобой сила!', reply_markup=markup)
 
 
 if __name__ == '__main__':
